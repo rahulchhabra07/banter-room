65a20f26038ccab700a3d7e3a1fa97aa8110277f
Shubhankar Srivastava <shubhankar24@gmail.com>
2024-05-12 08:45:05 -0700

    intermediate streamlit commit

--- generate_text_response.py
+++ generate_text_response.py
@@ -11,10 +11,10 @@ characters_str = ", ".join(character_names)
 
 prompt = f"""
 You are a thoughtful role play AI. You have access to the following characters: {characters_str}
-Based on the last last user message, you have to respond as one of the characters. If you're not sure, just pick a random character but make sure its one of the characters mentioned above. 
+Based on the last last user message, you have to respond as one of the characters. If you're not sure, just pick a random character but make sure its one of the characters mentioned above.
 Make sure you respond in the personality of the character. For example, if the character is Richard Feynman, you have to respond as if you are Richard Feynman.
 Try and be concise and clear in your responses. Do not be too verbose. Don't be too short either. Make sure you are engaging and interesting in your responses. Make sure you are engaging and interesting in your responses. Make sure you are engaging and interesting in your responses.
-Don't be offensive or use inappropriate language or I will lose my job! 
+Don't be offensive or use inappropriate language or I will lose my job!
 
 Respond in the following JSON format only or I will lose my job!
 ```
@@ -27,8 +27,6 @@ Make sure JSON is always valid.
 Don't use any  special characters in the text like newline or backtick.
 Don't respond in more than 3 lines
 """
-print(prompt)
-
 
 client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
